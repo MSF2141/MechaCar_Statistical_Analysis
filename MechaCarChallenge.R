6225f56d75f8e0f2e22151182271204adc1ecfe9
MSF2141 <114106019+MSF2141@users.noreply.github.com>
2023-02-16 16:31:38 -0500

    updated R script multiple linear regression model

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,2 +1,5 @@
 library(dplyr)
 MechaCar_mpg <- read.csv('MechaCar_mpg.csv') #import dataset
+lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
+     ground_clearance + AWD, data = MechaCar_mpg) # generate multiple linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg)) #generate summary statistics
