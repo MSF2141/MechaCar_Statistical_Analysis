770d185d28be3722116160c99a5620201df197cb
MSF2141 <114106019+MSF2141@users.noreply.github.com>
2023-02-20 10:40:33 -0500

    all & individual lots suspension coil ttest

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,13 @@ lot_summary <- Suspension_Coil_table %>%
   group_by(Manufacturing_Lot) %>%
   summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep') # generate summary statistics as a data frame based on the lot number
 lot_summary  
+t.test(log10(lot_summary$Mean),mu=mean(log10(total_summary$Mean)))
+all_lots.ttest <- t.test(x = lot_summary$Mean,  # Vector of data
+                         mu = 1500)  # Null: Mean is 1500 
+all_lots.ttest   # Print the result
+# Peform t-test on Lot 1
+t.test(subset(Suspension_Coil_table,Manufacturing_Lot=="Lot1")$PSI,mu = 1500) 
+# Peform t-test on Lot 2
+t.test(subset(Suspension_Coil_table,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+# Peform t-test on Lot 3
+t.test(subset(Suspension_Coil_table,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
